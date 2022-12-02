4331ddf3a7bc1fb3743843438255c286b919e694
esoe <igor_nikulin@mail.ru>
2022-12-02 09:00:23 +0300

    setup project

---
+++ src/main/java/ru/molokoin/Runner.java
@@ -0,0 +1,8 @@
+package ru.molokoin;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Вы исползуете интерпретатор скриптового языка ...");
+    }
+    
+}
