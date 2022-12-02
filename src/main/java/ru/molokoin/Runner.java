76b24513ef5856e4edb35d99429ff73fedde2b4b
esoe <igor_nikulin@mail.ru>
2022-12-02 12:59:58 +0300

    Runner.hello() Реализовано приветствие пользователя

--- src/main/java/ru/molokoin/Runner.java
+++ src/main/java/ru/molokoin/Runner.java
@@ -1,8 +1,32 @@
 package ru.molokoin;
 
+import java.io.File;
+/**
+ * Класс, запускает выполнение скриптового языка
+ */
 public class Runner {
+    /**
+     * Метод запускает выполнение программы "Scripting"
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Вы исползуете интерпретатор скриптового языка ...");
+        Runner.hello();
     }
-    
+
+    /**
+     * Метод выводит в консоль:<p>
+     * - приветствие пользователю<p>
+     * - руководство по использованию программы MANUAL.man<p>....
+     * @param readme
+     */
+    public static void hello(){
+        /**
+         * доработать:
+         * - чтобы путь брался из *.property файла;
+         */
+        File readme = new File("MANUAL.man");
+        System.out.println("Scripting (интерпретатор скриптового языка) запущен ...");
+        System.out.println("Прежде чем приступить к использованию пограммы, ОБЯЗВТЕЛЬНО_ОЗНАКОМЬТЕСЬ:");
+        System.out.println(Storage.get(readme));
+    }
 }
---
+++ src/main/java/ru/molokoin/Storage.java
@@ -0,0 +1,56 @@
+package ru.molokoin;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Storage {
+    /**
+     * Метод возвращает в виде строки текст файла<p>
+     * при чтении файлаидет проверка условий:<p>
+     * - наличие ссылки на файл в переменной<p>
+     * - наличие файла физически, по указанному адресу<p>
+     * - доступность файла для чтения<p>
+     * 
+     * @param sourceFile
+     * @return
+     */
+    public static String get(File sourceFile){
+        String line = "";
+        if((sourceFile != null)&&(sourceFile.exists())&&(sourceFile.canRead())){
+            try(FileReader reader = new FileReader(sourceFile)){
+                char[]buf = new char[5];
+                int c =0;
+                while((c=reader.read(buf))!=-1){
+                    line += new String(buf, 0, c);
+                }
+            }catch (IOException e) {
+                System.out.println("Файл по указанному пути отсутствует.");
+                System.out.println(e.getMessage());
+            }
+        }else{
+            throw new IllegalArgumentException("Ошибка: чтение файла " + sourceFile.getAbsolutePath() + " не удалось...");
+        }
+        return line;
+    }
+
+    /**
+     * Метод пишет в файл содержимое строки.<p>
+     * при записи в файл нужно проверить необходимые условия:<p>
+     * -<p>
+     * -<p>
+     * 
+     * @param content
+     * @param targetFile
+     */
+    public static void put(String content, File targetFile){
+        try(FileWriter writer = new FileWriter(targetFile, false)){
+            writer.append(content);
+        }catch(IOException e){
+            System.out.println("Файл по указанному пути отсутствует.");
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Созданный файл размещен по адресу: " + targetFile.getAbsolutePath());
+    }
+}
